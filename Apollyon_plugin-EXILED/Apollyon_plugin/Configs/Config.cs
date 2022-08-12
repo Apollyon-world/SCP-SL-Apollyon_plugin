8e668aece1f28923e98de0311a2ad33fd7628bd4
Apollyon_world <104727525+Apollyon-world@users.noreply.github.com>
2022-08-12 17:05:53 +0200

    Add files via upload

---
+++ Apollyon_plugin-EXILED/Apollyon_plugin/Configs/Config.cs
@@ -0,0 +1,35 @@
+using Exiled.API.Interfaces;
+using Apollyon_plugin.Configs;
+using System.ComponentModel;
+
+namespace Apollyon_plugin
+{
+    public sealed class Config : IConfig
+    {
+        private bool isEnabled = true;
+
+        public bool GetIsEnabled()
+        {
+            return isEnabled;
+        }
+
+        public void SetIsEnabled(bool value)
+        {
+            isEnabled = value;
+        }
+
+        [Description("Whether or not debug messages will be shown")]
+        public bool Debug { get; set; } = false;
+
+        [Description("Options for Manager O5 players")]
+        public SerpentsHandModifiers SerpentsHandModifiers { get; set; } = new SerpentsHandModifiers();
+
+        [Description("Options for Manager O5 spawn")]
+        public SpawnManager SpawnManager { get; set; } = new SpawnManager();
+
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("Sets the message for when the round starts.")]
+        public string RoundStartedMessage { get; set; } = "Site-69 est en alerte, veuillez évacuez immédiatement !";
+    }
+}
